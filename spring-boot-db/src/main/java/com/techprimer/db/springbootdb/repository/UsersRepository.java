6c31d36bccd839cea1cfedb15b84d575e4aa4e8a
punitkb <punitkumarbamaniya@gmail.com>
2018-10-09 12:02:40 +0530

    Update UsersRepository.java

--- spring-boot-db/src/main/java/com/techprimer/db/springbootdb/repository/UsersRepository.java
+++ spring-boot-db/src/main/java/com/techprimer/db/springbootdb/repository/UsersRepository.java
@@ -4,4 +4,6 @@ import com.techprimer.db.springbootdb.model.Users;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UsersRepository extends JpaRepository<Users, Integer> {
+  @Query(value =  "select id from users where id>?3", nativeQuery = true)
+  List<Users> findAllById(@Param("id") Integer id);
 }
